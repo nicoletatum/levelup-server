50cc606bdaa4bc240ae7a14cd6073153bacf270d
nicoletatum <nicoletatum@outlook.com>
2021-05-06 21:39:58 -0500

    ch7 complete

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -3,6 +3,8 @@ from django.db import models
 class Game(models.Model):
 
     name = models.CharField(max_length=50)
+    maker = models.CharField(max_length=50)
+    player = models.ForeignKey("Player", on_delete=models.CASCADE)
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)
     number_of_players = models.IntegerField()
     skill_level = models.IntegerField()
